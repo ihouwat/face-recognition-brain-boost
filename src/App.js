55fa37501f23cd6d7e8179d08518ff554292873b
Igor Houwat <58915539+ihouwat@users.noreply.github.com>
2020-06-30 17:38:18 -0400

    finishec profile modal

--- src/App.js
+++ src/App.js
@@ -142,6 +142,7 @@ class App extends Component {
         { isProfileOpen && 
           <Modal>
             <Profile 
+              loadUser={this.loadUser}
               isProfileOpen={isProfileOpen} 
               toggleModal={this.toggleModal} 
               user={user}  
